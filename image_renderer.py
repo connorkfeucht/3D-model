59cafb7c0e717e3010a2354b70f8344536652c20
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2025-05-09 11:05:16 -0700

    takes input argument for filename now

--- image_renderer.py
+++ image_renderer.py
@@ -1,46 +1,59 @@
 import h5py
+import sys
 import numpy as np
 import pyvista as pv
 
 # Using PyVista to render images of .hdf5 3D models
 
-# Open and locate the mesh groups 
-with h5py.File("Box.hdf5", "r") as f:
-    mesh_root = f["parts"]["part_001"]["mesh"] # contains subgroups 000, 001, ... which each contain one small mesh
 
-    # Load each sub-mesh into a PolyData and collect them
-    polys = []
-    for mesh_id in sorted(mesh_root.keys(), key=int): # get names like 000, 001, ... and sort them numerically
-        grp = mesh_root[mesh_id]
-        pts = grp["points"][...]       # shape (4,3), absolute coords of each vertex
-        tris = grp["triangle"][...]    # shape (2,3), how those vertices connect to form triangles
+def main(argc, argv):
+    if argc != 2:
+        print("Please specify an argument")
+        return
+    
+    filename = argv[1]
 
-        # VTK wants faces in "flat" format: [3, i0, i1, i2,  3, j0, j1, j2, …]
-        faces = np.hstack([
-            np.concatenate([[3], tri.astype(np.int64)])
-            for tri in tris
-        ]) # loop over each triangle, prepend the count 3, cast to int64, then horizontally stack them all into one flat array
+    # Open and locate the mesh groups 
+    with h5py.File(filename, "r") as f:
+        mesh_root = f["parts"]["part_001"]["mesh"] # contains subgroups 000, 001, ... which each contain one small mesh
 
-        poly = pv.PolyData(pts, faces) # builds PolyData objects, which are the small meshes in parts 000, 001, ...
-        polys.append(poly)
+        # Load each sub-mesh into a PolyData and collect them
+        polys = []
+        for mesh_id in sorted(mesh_root.keys(), key=int): # get names like 000, 001, ... and sort them numerically
+            grp = mesh_root[mesh_id]
+            pts = grp["points"][...]       # shape (4,3), absolute coords of each vertex
+            tris = grp["triangle"][...]    # shape (2,3), how those vertices connect to form triangles
 
-# Merge all sub-meshes into one (optional)
-mesh = polys[0]
-for poly in polys[1:]:
-    mesh = mesh.merge(poly)
+            # VTK wants faces in "flat" format: [3, i0, i1, i2,  3, j0, j1, j2, …]
+            faces = np.hstack([
+                np.concatenate([[3], tri.astype(np.int64)])
+                for tri in tris
+            ]) # loop over each triangle, prepend the count 3, cast to int64, then horizontally stack them all into one flat array
 
-# Render & save screenshot
-plotter = pv.Plotter(off_screen=True)
-plotter.add_mesh(mesh, color="white")
-plotter.set_background("black")
-plotter.camera_position = "iso" # can be "xy" "zy" or a point
-plotter.line_smoothing = True
-# might want to use image_scale at some point to make images smaller or larger
-plotter.show(screenshot="Box.png")
+            poly = pv.PolyData(pts, faces) # builds PolyData objects, which are the small meshes in parts 000, 001, ...
+            polys.append(poly)
 
-plotter = pv.Plotter(off_screen=True)
-plotter.add_mesh(mesh, color="white")
-plotter.set_background("black")
-plotter.camera_position = "xy"
-plotter.line_smoothing = True
-plotter.show(screenshot="Box2.png")
+    # Merge all sub-meshes into one (optional)
+    mesh = polys[0]
+    for poly in polys[1:]:
+        mesh = mesh.merge(poly)
+
+    # Render & save screenshot
+    plotter = pv.Plotter(off_screen=True)
+    plotter.add_mesh(mesh, color="white")
+    plotter.set_background("black")
+    plotter.camera_position = "iso" # can be "xy" "zy" or a point
+    plotter.line_smoothing = True
+    # might want to use image_scale at some point to make images smaller or larger
+    plotter.show(screenshot="Output.png")
+
+    plotter = pv.Plotter(off_screen=True)
+    plotter.add_mesh(mesh, color="white")
+    plotter.set_background("black")
+    plotter.camera_position = "xy"
+    plotter.line_smoothing = True
+    plotter.show(screenshot="Output2.png")
+
+if __name__ == "__main__":
+    main(len(sys.argv), sys.argv)
+
